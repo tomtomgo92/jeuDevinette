1f8719be4508f845a8f99baa8f2a1ce8cd642f2f
tomtomgo92 <tm.lepoleswebforce3@gmail.com>
2016-08-10 13:02:10 +0200

    Initial Commit

---
+++ devinette/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devinette
+//
+//  Created by thomas millet on 10/08/2016.
+//  Copyright © 2016 thomas millet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
